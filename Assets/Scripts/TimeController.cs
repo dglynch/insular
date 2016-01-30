b774a4b98e514743df007ac8425419262ee63696
Nicolas Lavallee <zalgryth@gmail.com>
2016-01-30 15:12:41 -0500

    Change time to be more granular.

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -2,39 +2,37 @@
 using System.Collections;
 
 public class TimeController : MonoBehaviour {
+    
+    private double secondOfDay = 21600;
 
+    private bool paused = false;
 
     /// <summary>
     /// Hour portion of time of day. Will be 0-23.
     /// </summary>
-    int hour = 6;
+    public int hour {
+        get {
+            return (int)(secondOfDay / 3600) % 24;
+        }
+    }
 
     /// <summary>
     /// Minute portion of time of day. Will be 0-59.
     /// </summary>
-    int minute = 0;
-
-    /// <summary>
-    /// Number of minutes incremented when event triggers.
-    /// </summary>
-    int minuteIncrement = 1;
-
-    /// <summary>
-    /// Real time until event gets triggered that updates the time.
-    /// </summary>
-    double secondsUntilIncrement = 1.0;
-
-
-    private double lastChange = 0.0;
-
-    void Update() {
-        if (Time.time - lastChange > secondsUntilIncrement) {
-            UpdateTime();
-
-            NewTimeEvents();
-
-            lastChange = Time.time;
+    public int minute {
+        get {
+            return (int)(secondOfDay / 60) % 60;
         }
+    }
+
+    public int speed = 60;
+
+    void FixedUpdate() {
+        var timeSinceLast = Time.fixedDeltaTime;
+        
+        UpdateTime(timeSinceLast);
+
+        NewTimeEvents();
     }
 
     void OnGUI() {
@@ -42,16 +40,12 @@ public class TimeController : MonoBehaviour {
     }
 
     public float GetFractionalTimeAfterDawn() {
-        return ((hour - 6) * 60 + minute) / (float) (24 * 60);
+        return (float)(secondOfDay - (6 * 60 * 60)) / (float) (24 * 60 * 60);
     }
 
-    // Some help from: http://forum.unity3d.com/threads/noob-question-help-with-in-game-clock.37668/
-    private void UpdateTime() {
-        minute += minuteIncrement;
-        if (minute >= 60) {
-            minute %= 60;
-            hour++;
-            hour = hour % 24;
+    private void UpdateTime(double timeSinceLast) {
+        if (!paused) {
+            secondOfDay += timeSinceLast * speed;
         }
     }
 
@@ -59,13 +53,13 @@ public class TimeController : MonoBehaviour {
     /// Time-specific events happen here.
     /// </summary>
     private void NewTimeEvents() {
-        if (isTime(0, 0)) {
+        if (isTimeLaterThan(24, 0)) {
             // You've reached a new day and you died.
         }
     }
 
-    private bool isTime(int hour, int minute) {
-        return hour == this.hour && minute == this.minute;
+    private bool isTimeLaterThan(int hour, int minute) {
+        return hour >= this.hour && minute >= this.minute;
     }
 
     // Converts the 12 hour time to 12-hour time.

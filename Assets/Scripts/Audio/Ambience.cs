eaa001d94b59736e54983b9ab0fe1fb6e8bb33c9
Sam Robinson <samuel.joseph.robinson@gmail.com>
2016-01-31 12:27:05 -0500

    Ambience Script

---
+++ Assets/Scripts/Audio/Ambience.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Ambience : AudioManager {
+
+	protected override void SetUpEvents() {
+		TimeController.RegisterSubscription(
+			new Func<bool>(() => TimeController.isTimeLaterThan(18, 0)),
+			new Action(() => {
+				audioSource.Stop();
+				audioSource.clip = nameToAudioClip["Ambience - Night"];
+				audioSource.Play();
+			}
+			));
+	}
+}

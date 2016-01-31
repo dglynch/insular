50aa1b6c3e82c8e183d74d2456a9a1685093e340
Alex Allin <allin.alexander@gmail.com>
2016-01-31 17:36:31 -0500

    Complete three pillar puzzle

---
+++ Assets/Scripts/Rituals/PillarController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillarController : MonoBehaviour {
+
+    public PillarTrioController TrioController;
+
+    public string PillarId;
+
+    public GameObject Light;
+
+    private bool active = false;
+
+    private bool resetting = false;
+
+    // Use this for initialization
+    void Start() {
+        Light.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (TrioController.puzzleFailed && resetting) {
+            //Remove glow
+            Debug.Log("Deactivate log");
+            Light.SetActive(false);
+            resetting = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider col) {
+        Debug.Log("Pillar trigger [" + col.gameObject.name + "]");
+        if (col.gameObject.name == "FPSController" && !active) {
+            Debug.Log("Pillar [" + PillarId + "] hit");
+            if (TrioController.PillarHit(PillarId)) {
+                //Play sound
+                //Glow
+                Light.SetActive(true);
+                active = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        if (col.gameObject.name == "FPSController" && active) {
+            Debug.Log("Pillar [" + PillarId + "] resetting");
+            active = false;
+            resetting = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Rituals/PillarTrioController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillarTrioController : MonoBehaviour {
+
+    private string code;
+
+    private bool codeComplete;
+
+    public GameObject TargetCube;
+
+    public GameObject[] Pillars;
+
+    public bool puzzleFailed;
+
+    // Use this for initialization
+    void Start () {
+        code = "";
+        codeComplete = false;
+        TargetCube.SetActive(false);
+        puzzleFailed = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool PillarHit(string pillarId) {
+        Debug.Log("Pillar [" + pillarId + "] hit");
+        puzzleFailed = false;
+        if (!codeComplete) {
+            code = code + pillarId;
+            if (code.Length < 3) {
+                Debug.Log("code [" + code + "]");
+                return true;
+            } else if (code == "bac") {
+                Debug.Log("code [" + code + "] complete");
+                TargetCube.SetActive(true);
+                return true;
+            } else {
+                code = "";
+                puzzleFailed = true;
+                //Play failed sound
+                Debug.Log("Code failed");
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}

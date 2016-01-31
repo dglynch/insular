d203711049aa9f334ec941464f840dc9abf15ccd
Dan Lynch <dglynch@dglynch.com>
2016-01-30 19:57:27 -0500

    Implement player drowning

---
+++ Assets/Scripts/PlayerDrownController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDrownController : MonoBehaviour {
+
+    public GameObject character;
+
+    public GameReset gameResetScript;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (character.transform.position.y < 9) {
+            gameResetScript.Reset();
+        }
+    }
+
+}

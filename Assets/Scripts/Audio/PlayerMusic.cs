81eb665dfe38c16550e58995588409ddbe40c19f
Sam Robinson <samuel.joseph.robinson@gmail.com>
2016-01-31 14:18:35 -0500

    Dynamic Night and Day Music and Ambience

--- Assets/Scripts/Audio/PlayerMusic.cs
+++ Assets/Scripts/Audio/PlayerMusic.cs
@@ -4,15 +4,14 @@ using System;
 
 public class PlayerMusic : AudioManager {
 
-    protected override void SetUpEvents() {
-        TimeController.RegisterSubscription(
-            new Func<bool>(() => TimeController.isTimeLaterThan(18, 0)),
-            new Action(() => {
-                // Set this up when the proper music is linked
-                //audioSource.Stop();
-                //audioSource.clip = nameToAudioClip["Ambience - Lake - Waves"];
-                //audioSource.Play();
-            }
-        ));
-    }
+	protected override void SetUpEvents() {
+		TimeController.RegisterSubscription(
+			new Func<bool>(() => TimeController.isTimeLaterThan(18, 0)),
+			new Action(() => {
+				audioSource.Stop();
+				audioSource.clip = nameToAudioClip["Creepy loop 1"];
+				audioSource.Play();
+			}
+			));
+	}
 }

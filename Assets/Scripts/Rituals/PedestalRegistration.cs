53c9f437f54d44117485119b108a683208cba6c2
Nicolas Lavallee <zalgryth@gmail.com>
2016-01-31 16:51:51 -0500

    Add pedestal logic to show the portal when all the cubes are placed on their correct pedestal.

---
+++ Assets/Scripts/Rituals/PedestalRegistration.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class PedestalRegistration : MonoBehaviour {
+
+    public PedestalRitual Ritual;
+
+    void OnTriggerEnter(Collider collider) {
+        if (IsMatch(collider)) {
+            Debug.Log("Cube matched.");
+            Ritual.AddMatch();
+        }
+    }
+
+    void OnTriggerExit(Collider collider) {
+        if (IsMatch(collider)) {
+            Debug.Log("Matched cube removed.");
+            Ritual.RemoveMatch();
+        }
+    }
+
+    private bool IsMatch(Collider collider) {
+        var cubeColour = collider.gameObject.name.Split('_').Last();
+        var pedestalColour = this.gameObject.name.Split('_').Last();
+        return cubeColour == pedestalColour;
+    }
+}
---
+++ Assets/Scripts/Rituals/PedestalRitual.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PedestalRitual : MonoBehaviour {
+
+    public GameObject Portal;
+
+    private int matchCount = 0;
+    private int winningMatchCount = 3;
+
+    void Start() {
+        Portal.SetActive(false);
+    }
+
+    public void AddMatch() {
+        matchCount++;
+
+        if (matchCount >= winningMatchCount) {
+            RitualComplete();
+        }
+    }
+
+    public void RemoveMatch() {
+        matchCount--;
+        // Prevent negative numbers
+        matchCount = Math.Max(matchCount, 0);
+    }
+
+    private void RitualComplete() {
+        Portal.SetActive(true);
+    }
+}

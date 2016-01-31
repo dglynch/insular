87cff1b7226abc4904faacfcf09a46647b4f9e6a
Nicolas Lavallee <zalgryth@gmail.com>
2016-01-31 15:29:54 -0500

    Add pillar walking ritual to make the green cube appear.

---
+++ Assets/Scripts/Rituals/PillarWalkingRitual.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Linq;
+
+public class PillarWalkingRitual : TriggerQueue {
+
+    public GameObject RewardCube;
+
+    void Start() {
+        maxItemsInQueue = 7;
+        RewardCube.SetActive(false);
+    }
+
+    protected override void Triggered() {
+        var ids = triggerQueue.Select(i => Convert.ToInt32(i.name.Split('_').Last())).ToList();
+        var id = ids.First();
+        var ritualCompleted = true;
+
+        for (var i = 0; i < maxItemsInQueue; i++, id = ((id + 1) % maxItemsInQueue)) {
+            if (ids[i] != id) {
+                ritualCompleted = false;
+                break;
+            }
+        }
+
+        if (ritualCompleted) {
+            triggerQueue.Clear();
+            Debug.Log(string.Format("{0}: PillarWalkingRitual complete", Time.time));
+            RewardCube.SetActive(true);
+        }
+    }
+}
